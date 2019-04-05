384448d672066cf3986cf2b2e9bb33e4fc5de849
Jennifer Lipps <jjlipps@gmail.com>
2019-04-04 20:46:44 -0500

    XHR request working

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('favorites');
+function successCode() {
+    console.log('success');
+}
+
+function failedCode() {
+    console.error('shit');
+}
+
+const getCityData = () => {
+  const cityRequest = new XMLHttpRequest();
+  cityRequest.addEventListener('load', successCode);
+  cityRequest.addEventListener('error', failedCode);
+  cityRequest.open('GET', './db/places.json');
+  cityRequest.send();
+  console.log(cityRequest);
+};
+
+const init = () => {
+    getCityData();
+};
+
+init();
