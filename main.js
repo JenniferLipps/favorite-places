de357ba8d1c23679d19eb0010b7cb2590a15f75a
Jennifer Lipps <jjlipps@gmail.com>
2019-04-06 13:19:35 -0500

    added cards and styling

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ const printToDom = (divId, textToPrint) => {
 const domStringBuilder = (allCities) => {
     let domString = '';
     allCities.forEach((city) => {
-        domString += `<h3>${city.cityName}</h3>`
+      domString += `<div class="card col-4 bg-secondary mb-3 border border-dark">`
+      domString += `<div class="card-body">`
+      domString += `<h2 class="card-header" id="card-head">${city.cityName}, ${city.cityState}</h2>`
+      domString += `<img src=${city.cityImage} alt=${city.cityName} class="img-fluid rounded" alt="Responsive image" id="city-image">`
+      domString += `<div id="best-text">`
+      domString += `<p>Best Restaurant: ${city.favoriteRestaurant}</p>`
+      domString += `<p>Best Bar: ${city.favoriteBar}</p>`
+      domString += `<p>Best Hotel: ${city.favoriteHotel}</p>`
+      domString += `<p>Best Tourist Attraction: ${city.favoriteTouristAttraction}</p>`
+      domString += `</div>`
+      domString += `</div>`
+      domString += `</div>`
     })
   printToDom('favCities', domString);
 };
